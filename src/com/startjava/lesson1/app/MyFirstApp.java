5426651cd6ecc8cf751390191bfa88aa138491f2
Romaro-i <IlyushinRV@gmail.com>
2021-08-04 13:55:48 +0300

    test work with Intellij IDEA

--- src/com/startjava/lesson1/app/MyFirstApp.java
+++ src/com/startjava/lesson1/app/MyFirstApp.java
@@ -2,6 +2,6 @@ package com.startjava.lesson1.app;
 
 public class MyFirstApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!!!");
+        System.out.println("Hello, World!");
     }
 }
